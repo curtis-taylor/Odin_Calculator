1a270abe69f48b80d98f42952b4293525baeb715
curtis taylor <curtis.t@gmail.com>
2025-10-15 03:24:38 -0400

    1st commit

---
+++ calc.js
@@ -0,0 +1,31 @@
+/*
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(op, a, b) {
+    
+} */
+
+const operations = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a,b) => a / b
+};
+
+const operation = 'add';
+const result = operations[operation](5, 3);
+console.log(result); // Output: 8
