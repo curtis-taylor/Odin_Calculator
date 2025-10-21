32a79602b433e0ae6ec47ce83c12fa05827eae71
curtis taylor <curtis.t@gmail.com>
2025-10-21 03:19:14 -0400

    Worked on processor function and write function

--- calc.js
+++ calc.js
@@ -30,6 +30,46 @@ const operation = 'add';
 const result = operations[operation](5, 3);
 console.log(result); // Output: 8
 
-function write(mode_state, calc_text, calc_char) {
-    
+function write_number(original_text, num_ch) {
+    if(original_text == '0') {
+            original_text = num_ch;
+    } else {
+    original_text = original_text + num_ch;
+    }
+
+    return original_text
+}
+
+function processor(mode_state, math_statement, calc_char) {
+
+
+    if(Object.keys(math_statement).length < 1) {
+                console.log("plus") 
+                math_statement['num1'] = screen_text.textContent;
+                math_statement['operator'] = " + "; 
+                // math_statement_list.push(screen_text.textContent);
+                // math_statement_list.push(" + ")
+                
+                console.log("+++")
+                
+                screen_text.textContent = screen_text.textContent + " + ";            
+            
+    } else {
+        let temp_list = screen_text.textContent.split(' ')
+        console.log(temp_list);
+        console.log("***")
+        math_statement['num2'] = temp_list[2];
+
+        math_statement['num1'] = operations['add'](parseFloat(math_statement['num1']), parseFloat(math_statement['num2']));
+        math_statement['op'] = "+";
+        
+        //math_statement_list[0] = operations['add'](parseFloat(math_statement_list[0]), parseFloat(temp_list[2]));
+        // math_statement_list[1] ="+";
+        
+        
+        screen_text.textContent = String(math_statement['num1']) + " + ";
+        math_statement['num2'] = 0;
+        console.log(math_statement_list);
+                
+    }
 }
