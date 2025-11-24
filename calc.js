8866540a43cfc246a3d8cddb2ced644d876a87a3
curtis-taylor <curtis.t@gmail.com>
2025-11-23 23:53:16 -0500

    almost done maybe

--- calc.js
+++ calc.js
@@ -5,15 +5,6 @@ function operation_func(a, b, op) {
     let t = 0;
     switch(op) {
         case "add":
-            /*
-            if(a.length + b.length > 17) {
-                t = BigInt(a) + BigInt(b);
-                console.log("op_func add add ");
-                console.log(b);
-
-            } else {
-                t = parseFloat(a) + parseFloat(b);
-            } */
             
             t = parseFloat(a) + parseFloat(b);
             break;
@@ -31,7 +22,6 @@ function operation_func(a, b, op) {
             break;
     }   
 
-    console.log(t + " ttttt")
     return t
 }
 
@@ -45,39 +35,40 @@ const operations = {
     divide: (a,b) => a / b
 };
 
-
+/*
 const operation = 'add';
 const result = operations[operation](5, 3);
-console.log(result); // Output: 8
+console.log(result); // Output: 8 */
 
 
 function control_text_size(text_elem) {
 
-       console.log(typeof(text_elem))
-        console.log(text_elem.textContent)
+     /*
+        console.log(typeof(text_elem))
+        console.log(text_elem.textContent) */
         let temp = text_elem.textContent.split(' ');
 
-        console.log(temp.length);
-        console.log("&&&&&&&&&&&&&&&&&&&&")
+       // console.log(temp.length);
+       // console.log("&&&&&&&&&&&&&&&&&&&&")
 
         if(text_elem.textContent.includes('BY ZERO')) { 
-            console.log("ZZZZZZZZZZZZZZZZ");
+            // console.log("ZZZZZZZZZZZZZZZZ");
             text_elem.style.setProperty('font-size', '30px');
         }
         else if(text_elem.textContent.length > 19) {
             text_elem.style.setProperty('font-size', '18px');
-            console.log("30 length " + text_elem.textContent)
+            // console.log("30 length " + text_elem.textContent)
             
             
             if(temp.length == 1) {
                 //text_elem.textContent = String((parseFloat(temp[0])) * 1 );
                 text_elem.textContent = String(parseFloat(temp[0]).toExponential());
-                console.log("control 1 " + text_elem.textContent);
+                // console.log("control 1 " + text_elem.textContent);
             
             } else if((temp[2] == '') && (temp[1] != '')) {
                 console.log("control 2")
                     temp[0] = String(parseFloat(temp[0]).toExponential())
-                    //temp[0] = String(parseFloat(temp[0]) * 1)
+                    
                     text_elem.textContent = temp[0] + " " + temp[1] + " ";
             } else {
                 console.log("^^^^^^^^^^^^^^^" + temp[2])
@@ -121,16 +112,15 @@ function write_char(original_text, num_ch) {
         if(!(original_text.includes('.'))) {
            
            original_text = original_text + num_ch;
-           console.log("%%%%%%% " + original_text)
+          // console.log("%%%%%%% " + original_text)
         }
     }
     else if(original_text == '0') {
             original_text = num_ch; 
 
-            console.log("zero suppose to returned " + original_text)
+            //console.log("zero suppose to returned " + original_text)
     } else {
         original_text = original_text + num_ch;
-        console.log(" original text  1" + original_text);
     }
 
     return original_text
@@ -147,7 +137,6 @@ function number_processor(operator_value, screen_text, num_ch) {
         return_obj.num1 = temp[0];
         return_obj.num2 = temp[2];
     
-        console.log("num2 button one " + return_obj.num1 + " " + operator_value + " " + return_obj.num2);
         if(return_obj.num2 != '0') {
             console.log("11%% " + operator_value);
             return_obj.num2 = write_char(return_obj.num2, num_ch);
@@ -157,11 +146,8 @@ function number_processor(operator_value, screen_text, num_ch) {
         if(!(return_obj.num1.includes('e'))) {
             return_obj.num1 = write_char(return_obj.text, num_ch)
             return_obj.text = return_obj.num1;
-            console.log("num1 button one " + return_obj.num1);
         }
     }
-
-    console.log(" obj " + return_obj.text);
     
     return [return_obj.text, return_obj.num1, return_obj.num2];
 
@@ -169,25 +155,19 @@ function number_processor(operator_value, screen_text, num_ch) {
 
 
 function op_processor(mode_state, math_statement_obj, text, operator_char, operation) {
-    console.log("xxxxxx  " + math_statement_obj)
 
     if(math_statement_obj.operator == '') {
-                console.log("lennkdjskdjskdjsk");
-                //math_statement_obj['num1'] = text;
-                math_statement_obj['operator'] = operator_char; 
-                math_statement_obj['operation'] = operation;
+        math_statement_obj['operator'] = operator_char; 
+        math_statement_obj['operation'] = operation;
 
-                mode_state.operator_can_be_entered = false;
-                // math_statement_list.push(screen_text.textContent);
-                // math_statement_list.push(" + ")      
-                
-                text = text + " " + operator_char + " " ;          
+        mode_state.operator_can_be_entered = false;
+        
+        text = text + " " + operator_char + " " ;          
             
     } else {
         let temp_list = text.split(' ')
 
         if((math_statement_obj['num2'] == '0') && (math_statement_obj['operation'] == "divide")) {
-            //math_statement_obj['num1'] = String(operations[math_statement_obj.operation](parseFloat(math_statement_obj['num1']), parseFloat(math_statement_obj['num2'])));
             math_statement_obj.num1 = String(operation_func(parseFloat(math_statement_obj.num1), parseFloat(math_statement_obj.num2), math_statement_obj.operation));
             math_statement_obj['answer'] = "";
             math_statement_obj['operator'] = "";
@@ -204,14 +184,15 @@ function op_processor(mode_state, math_statement_obj, text, operator_char, opera
             mode_state.operator_can_be_entered = false;
             text = String(math_statement_obj['answer']) + " " + operator_char + " ";
 
-            console.log("ANSWER : " + math_statement_obj['answer'])
-            console.log(math_statement_obj.num1 + math_statement_obj.num2);
+            //console.log("ANSWER : " + math_statement_obj['answer'])
+            //console.log(math_statement_obj.num1 + math_statement_obj.num2);
 
         }
 
+        /*
         console.log(text);
         console.log(math_statement_obj)
-        console.log("***  " + math_statement_obj.operation) 
+        console.log("***  " + math_statement_obj.operation) */
         
         math_statement_obj.num2 = "";
                 
